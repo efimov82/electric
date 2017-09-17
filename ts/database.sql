36db3202cefcb87f7f06e157380584490e4dc2ff
efimov82 <efimov82@gmail.com>
2017-09-17 11:06:24 +0700

    added composer.json

---
+++ ts/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users_results` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `date_create` TIMESTAMP NULL,
+  `scores` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
