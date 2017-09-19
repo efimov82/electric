102008a8e2cb6e216ce659e8873521f5d5587f64
efimov82 <efimov82@gmail.com>
2017-09-19 14:19:23 +0700

    fix lamps images for 8x8 game

---
+++ www/_html/header.php
@@ -0,0 +1,37 @@
+<head>
+  <title>Electric</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+
+
+  <link href="css/style.css?v=3" rel="stylesheet">
+  <link href="css/style<?php echo $gameSize ?>.css?v=3" rel="stylesheet">
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="/js/functions.js?v=3"></script>
+  <!-- My flip counter script, REQUIRED -->
+	<script type="text/javascript" src="/js/flipcounter.min.js"></script>
+	<!-- Style sheet for the counter, REQUIRED -->
+	<link rel="stylesheet" type="text/css" href="/css/counter.css" />
+
+  <!-- Global Site Tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106534993-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+
+    gtag('config', 'UA-106534993-1');
+  </script>
+
+
+</head>
--- www/index.php
+++ www/index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include '_html/header.html';
 include_once '../bootstrap.php';
-
+include '_html/header.php';
 ?>
 <body>
 
